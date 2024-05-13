f343bd515197c067b36b4402291d4dcd14f659e2
bion0625 <bion0625@gmail.com>
2024-05-14 01:05:36 +0900

    로그인, 로그아웃 접근 제한 및 로깅 기능 추가

--- src/main/java/com/board/portfolio/controller/IndexController.java
+++ src/main/java/com/board/portfolio/controller/IndexController.java
@@ -1,5 +1,9 @@
 package com.board.portfolio.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+
     @GetMapping
-    public String index() {
+    String index() {
         System.out.println("test");
         return "index";
     }
 
     @GetMapping("index")
-    public String reIndex() {
+    String reIndex() {
         System.out.println("test2");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        this.logoutHandler.logout(request, response, authentication);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/test")
+    String test() { // 로그인 안 하면 접근 불가
         return "index";
     }
 }
