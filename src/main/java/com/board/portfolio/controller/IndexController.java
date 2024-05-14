6b5bcdda6c6e4d0181cfe31efb895602d9c38867
bion0625 <bion0625@gmail.com>
2024-05-14 17:46:16 +0900

    lombok 추가 및 로그인 여부 확인 로직 root url에 추가

--- src/main/java/com/board/portfolio/controller/IndexController.java
+++ src/main/java/com/board/portfolio/controller/IndexController.java
@@ -1,16 +1,24 @@
 package com.board.portfolio.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     /*
      * 로그인 안 해도 접근 가능
      */
     @GetMapping
-    String root() {
+    String root(Authentication authentication) {
+        if (authentication != null) {
+            UserDetails details = (UserDetails) authentication.getPrincipal();
+            log.info("user: {}", details);
+        } else log.info("no user");
         return "index";
     }
 
